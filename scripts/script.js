b9d1c0823720b808d9724aa07384441f89b24bcf
Glenmore Vinoya <glenmore@pinoyak.com>
2020-11-26 01:30:10 -0900

    restructured account movements data

--- scripts/script.js
+++ scripts/script.js
@@ -10,35 +10,56 @@
 const account1 = {
   owner: 'Michael Elliott',
   movements: [
-    {amount: 200, date: 927923563042},
-    {amount: 450, date: 622250976809},
-    {amount: -400, date: 1551287510555},
-    {amount: 3000, date: 491146958534},
-    {amount: -650, date: 167202734658},
-    {amount: -130, date: 124764445521},
-    {amount: 70, date: 1170976427420},
-    {amount: 1300, date: 1206456104396}],
+    {amount: 200, date:  124764445521},
+    {amount: 450, date:  167202734658},
+    {amount: -400, date: 491146958534},
+    {amount: 3000, date: 622250976809},
+    {amount: -650, date: 927923563042},
+    {amount: -130, date: 1170976427420},
+    {amount: 70, date:   1206456104396},
+    {amount: 1300, date: 1551287510555}],
   interestRate: 1.2, // %
   pin: 1111,
 };
 
 const account2 = {
   owner: 'Glenmore Vinoya',
-  movements: [5000, 3400, -150, -790, -3210, -1000, 8500, -30],
+  movements: [
+    {amount: 5000, date:  14852956653},
+    {amount: 3400, date:  39180437308},
+    {amount: -150, date:  50224823729},
+    {amount: -790, date:  492545040256},
+    {amount: -3210, date: 800723321732},
+    {amount: -1000, date: 816880471563},
+    {amount: 8500, date:  1499440697851},
+    {amount: -30, date:   1576576449756}],
   interestRate: 1.5,
   pin: 2222,
 };
 
 const account3 = {
   owner: 'Mauricio Moreno',
-  movements: [200, -200, 340, -300, -20, 50, 400, -460],
+  movements: [
+    {amount: 200, date:  208161906953},
+    {amount: -200, date: 307525740398},
+    {amount: 340, date:  615210887681},
+    {amount: -300, date: 633796865420},
+    {amount: -20, date:  954498604344},
+    {amount: 50, date:   1215770471451},
+    {amount: 400, date:  1248436250543},
+    {amount: -460, date: 1537121713867}],
   interestRate: 0.7,
   pin: 3333,
 };
 
 const account4 = {
   owner: 'Shay Rosner',
-  movements: [430, 1000, 700, 50, 90],
+  movements: [
+    {amount: 430, date: 67771266395},
+    {amount: 1000, date:424462755523},
+    {amount: 700, date: 931187703268},
+    {amount: 50, date:  1399198087008},
+    {amount: 90, date:  1464350286837}],
   interestRate: 1,
   pin: 4444,
 };
@@ -133,7 +154,8 @@ btnLogin.addEventListener('click', e => {
       let date = new Date(transaction.date).toUTCString();
 
       if (transaction.amount > 0) {
-
+        deposits.push(transaction.amount);
+        depositsValue = Balance.add(deposits);
         movementsBlock.innerHTML +=
         ` <div class="movements__row">
           <div class="movements__type movements__type--deposit"> Transaction #${i+1}\ndeposit </div>
@@ -144,6 +166,8 @@ btnLogin.addEventListener('click', e => {
 
       }
       if (transaction.amount < 0) {
+        withdrawals.push(transaction.amount);
+        withdrawalsValue = Balance.add(withdrawals);
         movementsBlock.innerHTML +=
         ` <div class="movements__row">
           <div class="movements__type movements__type--withdrawal"> Transaction #${i+1}\nwithdrawal </div>
